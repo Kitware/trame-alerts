aeb10e3d3fee08ebb64d4386bb51482b8730e292
Alessandro Genova <alessandro.genova@kitware.com>
2025-02-13 15:00:01 -0500

    Add vuetify UI implementation of AlertsPopup, AlertsList, AlertsCount

--- tests/test_import.py
+++ tests/test_import.py
@@ -11,3 +11,10 @@ def test_quasar_import():
 
     # Widgets are also importable via the trame namespace
     from trame.widgets.alerts_quasar import AlertsPopup, AlertsList, AlertsCount  # noqa: F401,F811
+
+
+def test_vuetify_import():
+    from trame_alerts.vuetify.widgets import AlertsPopup, AlertsList, AlertsCount  # noqa: F401
+
+    # Widgets are also importable via the trame namespace
+    from trame.widgets.alerts_vuetify import AlertsPopup, AlertsList, AlertsCount  # noqa: F401,F811
---
+++ vue-components/vuetify/src/components/AlertsCount.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import type { Alert } from "@/types";
+
+interface Props {
+  alerts: Alert[];
+}
+
+type Events = {
+  click: [];
+};
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<Events>();
+
+const nAlerts = computed(() => props.alerts.length);
+
+function onClick() {
+  emit("click");
+}
+</script>
+
+<template>
+  <v-btn @click="onClick" stacked rounded>
+    <v-badge v-if="nAlerts > 0" color="error" :content="nAlerts">
+      <v-icon>mdi-bell-outline</v-icon>
+    </v-badge>
+    <v-icon v-else>mdi-bell-outline</v-icon>
+  </v-btn>
+</template>
